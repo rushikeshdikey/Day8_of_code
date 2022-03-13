a1e41b49c3293f8f692ff63bd31c5243eb3ea6e2
rushikesh <dikeyrushikesh@gmail.com>
2022-03-13 12:41:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Day8_of_code
+
+* Basic of Functions
+* Created calc with functions
+* Solved the maze problems by showing the robot path
---
+++ main.py
@@ -0,0 +1,7 @@
+# start to python day-8 code
+# starting code is basics learned
+# main project code at end
+
+# Author: Rushikesh Dikey
+# Date: 12-03-2022
+
